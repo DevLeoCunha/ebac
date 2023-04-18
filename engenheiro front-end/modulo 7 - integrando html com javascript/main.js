73eec3b191c6a0dca6a59ddf386a20b92ac1d4e5
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2023-04-18 18:41:17 -0300

    .

--- engenheiro front-end/modulo 7 - integrando html com javascript/main.js
+++ engenheiro front-end/modulo 7 - integrando html com javascript/main.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form-deposito')
+const form = document.getElementById('form-deposito');
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
@@ -6,5 +6,24 @@ function validaNome(nomeCompleto) {
 }
 
 form.addEventListener('submit', function(e) {
-    
+    let formEValido = false;
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario}`;
+
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido) {
+        alert(mensagemSucesso);
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+    } else {
+        alert("O nome não está completo");
+    }
 })
+
+console.log(form);
