d48539141957e791773fb0e209e42c7358a05dd1
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2023-04-18 18:47:14 -0300

    .

--- engenheiro front-end/modulo 7 - integrando html com javascript/main.js
+++ engenheiro front-end/modulo 7 - integrando html com javascript/main.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', function(e) {
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario}`;
+    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;
 
     formEValido = validaNome(nomeBeneficiario.value)
     if (formEValido) {
