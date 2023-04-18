5ea6ed991cf420f7b14b996bc506e4da54bd91df
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2023-04-18 18:29:42 -0300

    .

---
+++ engenheiro front-end/modulo 7 - integrando html com javascript/main.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form-deposito')
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function(e) {
+    
+})
