7edb5f840e16978aab92abef348cd0c5ecb24eb9
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2023-05-01 18:41:26 -0300

    Update main.js

---
+++ engenheiro front-end/modulo 9 - conhecendo o jquery/jquery/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('header button').click(function(){
+        alert("Expandir formulário")
+    })
+
+    $('form').on('submit', function(e) {
+        console.log("submit");
+        e.preventDefault();
+
+    })
+})
+
