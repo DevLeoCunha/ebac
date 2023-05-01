3162b6053d4add1aa594cb51894f54ee398343f6
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2023-05-01 18:57:08 -0300

    .

--- engenheiro front-end/modulo 9 - conhecendo o jquery/jquery/main.js
+++ engenheiro front-end/modulo 9 - conhecendo o jquery/jquery/main.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
     $('header button').click(function(){
-        alert("Expandir formulário")
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
     })
 
     $('form').on('submit', function(e) {
         console.log("submit");
         e.preventDefault();
-
     })
 })
 
