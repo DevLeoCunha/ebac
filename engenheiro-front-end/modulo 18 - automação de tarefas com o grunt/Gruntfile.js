bedadfc279ee006b16eb6357e49a85553c7e868d
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2023-05-26 00:23:36 -0300

    .

--- engenheiro-front-end/modulo 18 - automação de tarefas com o grunt/Gruntfile.js
+++ engenheiro-front-end/modulo 18 - automação de tarefas com o grunt/Gruntfile.js
@@ -1,5 +1,47 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css':'main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'main2.css': 'main.scss'
+                }
+            }
+        },
+        concurrent: {
+            target: ['olaGrunt', 'less', 'sass']
+        }
     })
+
+    grunt.registerTask('olaGrunt', function() {
+        const done = this.async();
+        setTimeout(function() {
+            console.log('Olá Grunt');   
+            done();
+        }, 3000);
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks ('grunt-concurrent');
+
+    grunt.registerTask('default', ['concurrent']);
 } 
